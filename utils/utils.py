5168f208ae8a91ab7e45a15c3ede4cf8fce5ad67
mfsuve <suve@itu.edu.tr>
2020-05-20 12:09:50 +0300

    Moved nodered_function decorator to Node class

--- utils/utils.py
+++ utils/utils.py
@@ -8,53 +8,57 @@ import logging
 log = logging.getLogger('nodered')
 
 
-# ! Function having this decorator should not return
-# ! a list for one output, it should be put in a tuple
-# * single output for single output nodes
-# * list of outputs for multiple output nodes
-##########################################
-# TODO: Put the output in an Output object to store info like
-# TODO: if it is an error, you can also itearte the output
-# TODO: inside the function_end function
-def nodered_function(**input_names):
-    # if input_names is None:
-    #     input_names = {}
-    def decorator(func):
-        @wraps(func)
-        def wrapper(input_queue=None, **kwargs):
-            try:
+# # ! Function having this decorator should not return
+# # ! a list for one output, it should be put in a tuple
+# # * single output for single output nodes
+# # * list of outputs for multiple output nodes
+# ##########################################
+# # TODO: Put the output in an Output object to store info like
+# # TODO: if it is an error, you can also itearte the output
+# # TODO: inside the function_end function
+# def nodered_function(*inputs):
+#     # stream_data = any(isinstance(C, Stream) for C in inputs)
+#     inputs = {C.name.lower(): C for C in inputs} # Assuming all input classes have the same name as the topics
+#     def decorator(func):
+#         @wraps(func)
+#         def wrapper(input_queue=None, **kwargs):
+#             try:
             
-                myprint(func.__name__, f'kwargs: {type(kwargs)}', kwargs, f'input names: {type(input_names)}', input_names)
+#                 myprint(func.__name__, f'kwargs:', kwargs, f'inputs:', inputs)
                 
-                if input_queue is not None:
-                    for i in range(len(input_names)):
-                        myprint(func.__name__, f'waiting for input...')
-                        topic, data = input_queue.get()
-                        myprint(func.__name__, f'Got {i+1}. input:', 'topic:', topic, 'data:', data)
-                        if data == 'error':
-                            return None
-                        kwargs.update(zip(input_names[topic], data))
+#                 if input_queue is not None:
+#                     for i in range(len(inputs)):
+#                         myprint(func.__name__, f'waiting for input...')
+#                         topic, data = input_queue.get()
+#                         myprint(func.__name__, f'Got {i+1}. input:', 'topic:', topic, 'data:', data)
+#                         if data == 'error':
+#                             return None
+                        
+#                         # inputs = (Data, Model)
+#                         # topic, data = model, SVM
+                        
+#                         kwargs.update(inputs[topic].format(data))
                 
-                returned = None
-                try:
-                    returned = func(**kwargs)
-                except Exception as e:                                  # In case of an exception
-                    return repr(e) + '\n' + format_exc()
-                if returned is None:                                    # In case nothing returns
-                    return []
-                if not isinstance(returned, list):                      # In case one out returns
-                    if isinstance(returned, tuple):
-                        returned = [returned]
-                    else:
-                        returned = [(returned,)]
-                return returned
+#                 returned = None
+#                 try:
+#                     returned = func(**kwargs)
+#                 except Exception as e:                                  # In case of an exception
+#                     return repr(e) + '\n' + format_exc()
+#                 if returned is None:                                    # In case nothing returns
+#                     return []
+#                 if not isinstance(returned, list):                      # In case one out returns
+#                     if isinstance(returned, tuple):
+#                         returned = [returned]
+#                     else:
+#                         returned = [(returned,)]
+#                 return returned
             
-            except Exception:
-                myprint(format_exc())
-                return []
+#             except Exception:
+#                 myprint(format_exc())
+#                 return []
             
-        return wrapper
-    return decorator
+#         return wrapper
+#     return decorator
 
 
 def myprint(*args, end='', sep='\n'):
@@ -66,3 +70,11 @@ def myprint(*args, end='', sep='\n'):
             final_string += pformat(s) + sep
     final_string += end
     log.warning(final_string)
+   
+ 
+def make_list_of_tuples(val):
+    if not isinstance(val, list):
+        if isinstance(val, tuple):
+            return [val]
+        else:
+            return [(val,)]
