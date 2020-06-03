cd2c4b7bcb7ed1c9464c74ecf1160ad3a49ebd25
mfsuve <suve@itu.edu.tr>
2020-06-03 10:12:40 +0300

    Implemented SimpleDataset

--- utils/net/utils.py
+++ utils/net/utils.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn, optim
+from torch.nn import functional as F
 from abc import ABCMeta, abstractmethod
+from torch.utils.data import Dataset
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -9,13 +11,31 @@ class Trainable(nn.Module, metaclass=ABCMeta):
     def __init__(self):
         super().__init__()
         self.optimizer = None
-        self.loss = None
 
     @abstractmethod
-    def train_step(self, epochs, batch_size):
-        if self.optimizer is None or self.loss is None:
+    def train_step(self):
+        if self.optimizer is None:
             raise AttributeError("'optimizer' and 'loss' attributes need to be initialized before training.")
+        self.train()
+        self.optimizer.zero_grad()
+
 
 
 def to_tensor(tensor):
-    return torch.from_numpy(tensor).float().to(device)
+    return torch.from_numpy(tensor).float()
+
+
+class SimpleDataset(Dataset):
+    def __init__(self, X, y):
+        super().__init__()
+        self.X = to_tensor(X)
+        self.y = to_tensor(y)
+        
+    def __len__(self):
+        return self.X.size()[0]
+    
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+        return self.X[index, :], self.y[index]
+        
