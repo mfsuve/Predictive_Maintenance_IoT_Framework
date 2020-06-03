fd0ac509c3eaab0616f559a10c62483d19888dd0
mfsuve <suve@itu.edu.tr>
2020-06-03 10:14:17 +0300

    Implemented loss and train for Classifier

--- utils/net/classifier.py
+++ utils/net/classifier.py
@@ -1,6 +1,9 @@
+from utils.utils import myprint as print
+
 from utils.net.utils import Trainable
 from utils.net.linear.mlp import MLP
 from torch import nn, optim
+from torch.nn import functional as F
 
 class Classifier(Trainable):
     def __init__(self, input_size, classes, layers, hid_size=1000, hid_smooth=None, drop=0,
@@ -12,19 +15,51 @@ class Classifier(Trainable):
         if layers < 1:
             raise ValueError("The classifier needs to have at least 1 fully-connected layer.")
 
-        # ? Might not necessary
-        # Can create Replayer class
-        self.replay_targets = 'soft'
-        self.KD_temp = 2.0
-        
         ######------SPECIFY MODEL------######
         
         self.mlp = MLP(input_size=input_size, output_size=classes, layers=layers, hid_size=hid_size,
                        hid_smooth=hid_smooth, batch_norm=batch_norm, gated=gated, excitability=excitability,
                        excit_buffer=excit_buffer, drop=drop, nl=nl, output='excitable')
         
+        self.loss = nn.CrossEntropyLoss(reduction='mean')
         
-    def train_step(self, epochs, batch_size):
-        super().train_step(epochs, batch_size)
         
+    def forward(self, x):
+        return self.mlp(x)
+    
+    
+    def dist_loss(self, scores, target_scores, T=2.):
+        """Compute knowledge-distillation (KD) loss given [scores] and [target_scores].
+
+        Both [scores] and [target_scores] should be tensors, although [target_scores] should be repackaged.
+        'Hyperparameter': temperature"""
+
+        log_scores_norm = F.log_softmax(scores / T, dim=1)
+        targets_norm = F.softmax(target_scores / T, dim=1)
+
+        # Calculate distillation loss (see e.g., Li and Hoiem, 2017)
+        KD_loss_unnorm = -(targets_norm * log_scores_norm)
+        KD_loss_unnorm = KD_loss_unnorm.sum(dim=1)                      #--> sum over classes
+        KD_loss_unnorm = KD_loss_unnorm.mean()                          #--> average over batch
+
+        # normalize
+        KD_loss = KD_loss_unnorm * T**2
+
+        return KD_loss
+    
+        
+    def train_step(self, x, y, gen_x, gen_y, scores, gen_scores, rnt):
+        super().train_step()
+        
+        y_hat = self(x)
+        print(f'In Classifier', f'y.dtype: {y.dtype}', f'y_hat.dtype: {y_hat.dtype}')
+        total_loss = self.loss(y_hat, y)
+        
+        if gen_x is not None:
+            y_hat = self(gen_x)
+            replay_loss = self.dist_loss(y_hat, gen_scores, T=2.0)
+            total_loss = rnt * total_loss + (1 - rnt) * replay_loss
+        
+        total_loss.backward()
+        self.optimizer.step()
         
