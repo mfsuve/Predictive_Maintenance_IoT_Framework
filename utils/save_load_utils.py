1441090d4f4d256afd867e5d2dfa485e1cfa8078
mfsuve <suve@itu.edu.tr>
2020-09-21 12:32:40 +0300

    prefix defaults to datetime string

--- utils/save_load_utils.py
+++ utils/save_load_utils.py
@@ -8,10 +8,13 @@ def get_path_to_model(folder, model_name):
     return make_sure_folder_exists(os.path.join(folder, model_name, datetime.now().strftime("%d-%m-%Y_%H-%M-%S")))
 
 
-def add_prefix(prefix, name, path=None):
+def add_prefix(name, prefix=None, path=None):
+    '''Default prefix is datetime string'''
+    if prefix is None:
+        prefix = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
     if not isinstance(prefix, str):
         raise TypeError('prefix needs to be a string')
-    if prefix.strip() == '':
+    if prefix.strip() != '':
         prefix = prefix.strip().replace(' ', '_')
         if prefix.endswith('_'):
             name = f'{prefix}{name}'
@@ -20,7 +23,7 @@ def add_prefix(prefix, name, path=None):
     if path is None:
         return name
     else:
-        return os.path.join(path, name)
+        return os.path.join(make_sure_folder_exists(path), name)
 
 
 def get_file_to_load(path, file_name='model.pkl'):
