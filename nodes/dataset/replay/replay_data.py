3fce85b346826c17a170aa4c4b2d121400bc97a5
mfsuve <suve@itu.edu.tr>
2020-09-28 14:20:34 +0300

    Replaying the whole data for Flexsim (not just y)

--- nodes/dataset/replay/replay_data.py
+++ nodes/dataset/replay/replay_data.py
@@ -3,7 +3,7 @@ import sys
 import json
 import numpy as np
 
-from utils.utils import myprint as print
+from utils.utils import myprint as print, combine_data
 from utils.node import Data
 from utils.io import InputType
 
@@ -16,22 +16,20 @@ class ReplayData(Data):
         self.q = deque()
 
     def function(self, data):
-        # Currently just storing y
+        if data.type != InputType.DATA and data.type != InputType.NODERED:
+            raise TypeError(f"Input needs to be either from a data node or from nodered but got from '{data.type.name.lower()}'")
+        
         if data.type == InputType.NODERED: # Coming from nodered
             try:
                 replayed = self.q.popleft()
                 self.send_nodered(replayed)
                 print('ReplayData | Replayed data', f'ReplayData | Current Lenght: {len(self.q)}')
             except IndexError:
-                # TODO: Fix this, what to do if there is no data to be replayed?
+                self.warning('There is no data to be replayed')
                 print('ReplayData | Tried to replay but there is no data', f'ReplayData | Current Lenght: {len(self.q)}')
         else:
-            if data.type != InputType.DATA:
-                raise TypeError(f"Input needs to be a data coming from a data node but got '{data.type.name.lower()}'")
-            _, y = data.get()
-            if y is not None:
-                self.q.extend(y)
-                print('ReplayData | Stored data', f'ReplayData | Current Lenght: {len(self.q)}')
+            self.q.extend(combine_data(*data.get()))
+            print('ReplayData | Stored data', f'ReplayData | Current Lenght: {len(self.q)}')
                 
         self.status(f'data size: {len(self.q)}')
     
